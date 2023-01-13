851c91d450e544a2b3d6c5f2666a6e08d6102580
Orcun Sarmis <yorcunsarmis@gmail.com>
2023-01-13 21:32:18 +1000

    adding components and css

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+      <div>Amount</div>
+      </div>
+    </>
+  );
+}
+
+export default ExpenseItem;
