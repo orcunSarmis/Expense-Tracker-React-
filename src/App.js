d81e5e435f77675347f6150008f7d5e975fedc8f
Orcun Sarmis <yorcunsarmis@gmail.com>
2023-01-26 20:56:22 +1000

    adding props child to parent

--- src/App.js
+++ src/App.js
@@ -29,9 +29,14 @@ const App = (props) => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App');
+    console.log(expense);
+  };
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
   );
