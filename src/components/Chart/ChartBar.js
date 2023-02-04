d07af3c04a398649b768118ae996757b0f4f75f2
Orcun Sarmis <yorcunsarmis@gmail.com>
2023-02-04 21:06:34 +1000

    adding chart component code

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,9 +1,20 @@
-import './ChartBar.css';
+import "./ChartBar.css";
 
-const ChartBar = () => {
-  return (
-    <div>ChartBar</div>
-  )
+const ChartBar = (props) => {
+let barFillHeight = '0%';
+
+if (props.max > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100 + '%')
 }
 
-export default ChartBar
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div className="chart-bar__fill" style={{ height: barFillHeight }}></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div>
+  );
+};
+
+export default ChartBar;
